0a016bdc096d3765b8463097d3c18f3891242a25
mytiqgfo <nohajhohs@gmail.com>
2022-06-29 21:48:04 +0500

    added english translation

---
+++ js/translation.js
@@ -0,0 +1,237 @@
+var app = {};
+app.translations = {
+  en: {
+    aboutNav: "About",
+    benefitsNav: "Benefits",
+    servicesNav: "Services",
+    contactsNav: "Contacts",
+
+    aboutNavMob: "About",
+    benefitsNavMob: "Benefits",
+    servicesNavMob: "Services",
+    contactsNavMob: "Contacts",
+
+    customsBrokerage: "Customs brokerage services",
+    competentSolutions: "Competent solutions for foreign economic activity",
+    languageSwitch: "Ru",
+
+    customsProblems: "Possible problems during customs clearance",
+    problemsProtocol: "Protocol on violation of customs rules",
+    transportDowntime: "Transport downtime costs",
+    corruption: "Corruption",
+    increaseCustomsValue: "Unjustified increase in customs value",
+    codeChange: "Product code change",
+
+    customsProblems1: "Possible problems during customs clearance",
+    problemsProtocol1: "Protocol on violation of customs rules",
+    transportDowntime1: "Transport downtime costs",
+    corruption1: "Corruption",
+    increaseCustomsValue1: "Unjustified increase in customs value",
+    codeChange1: "Product code change",
+
+    customsProblems2: "Possible problems during customs clearance",
+    problemsProtocol2: "Protocol on violation of customs rules",
+    transportDowntime2: "Transport downtime costs",
+    corruption2: "Corruption",
+    increaseCustomsValue2: "Unjustified increase in customs value",
+    codeChange2: "Product code change",
+
+    avoidThis: "Do you want to avoid this?",
+
+    aboutSection: "About Us",
+
+    about1: "Ruz Consulting is a team of professionals with extensive experience in foreign economic activity.",
+    about2: "We know everything about the customs clearance of any goods and vehicles.",
+    about3: "Due to the extensive experience of our specialists, we know all the features of the work of customs authorities and promptly monitor changes in the laws of different countries.",
+    about4: "And all in order to provide quality services to you here and now",
+
+    benefitsSection1: "Benefits of",
+    benefitsSection2: "working with us",
+
+    benefit1: "Over 15 years of experience",
+    benefit2: "We accept payment for services after quality performance",
+    benefit3: "We represent your interests at all stages of work",
+    benefit4: "We arrange any goods completely",
+    benefit5: "We offer favorable conditions for regular customers",
+    benefit6: "Affordable price",
+    benefit7: "We will help you optimize taxation",
+
+    sevicesSection1: "What",
+    sevicesSection2: "is included",
+    sevicesSection3: "in service?",
+
+    services1: "Consultations on any issues of customs clearance and tax optimization",
+    services2: "Obtaining all necessary documents from regulatory authorities",
+    services3: "Goods classification",
+    services4: "Declaring your goods to customs authorities",
+    services5: "Representation of your interests at all stages of customs clearance",
+
+    processesSection1: "Work",
+    processesSection2: "Process",
+
+    processesStep1: "Step 1",
+    processesStep1d: "We conclude a contract for the provision of services and receive documents for the cargo",
+    processesStep2: "Step 2",
+    processesStep2d: "We analyze documents and determine measures for tariff and non-tariff regulation",
+    processesStep3: "Step 3",
+    processesStep3d: "Obtaining permits from other regulatory authorities",
+    processesStep4: "Step 4",
+    processesStep4d: "We arrange the cargo",
+
+    needABroker: "Need a reliable customs broker?",
+    ruzHappyToHelp: "Ruz Consulting is always happy to help!",
+    copyrightRuz: "Ⓒ Ruz Consulting",
+    llcRuz: "LLC “Ruz Consulting”",
+    adressRuz: "Tajikistan Republic <br> 734000 Dushanbe <br>  5/5 str. N.Muhammad <br> 233 office, BC “Regus”",
+    yourCargo: "Your cargo - our concern!",
+  },
+  ru: {
+    aboutNav: "О нас",
+    languageSwitch: "En",
+    benefitsNav: "Преимущества",
+    servicesNav: "Услуги",
+    contactsNav: "Контакты",
+
+    aboutNavMob: "О нас",
+    benefitsNavMob: "Преимущества",
+    servicesNavMob: "Услуги",
+    contactsNavMob: "Контакты",
+
+    customsBrokerage: "Таможенно-брокерские услуги",
+    competentSolutions: "Грамотные решения для внешнеэкономической деятельности",
+
+    customsProblems: "Возможные проблемы при таможенном оформлении",
+    problemsProtocol: "Протокол о нарушении таможенных правил",
+    transportDowntime: "Затраты при простое транспорта",
+    corruption: "Коррупция",
+    increaseCustomsValue: "Необоснованное повышение таможенной стоимости",
+    codeChange: "Изменение кода товара",
+    
+    customsProblems1: "Возможные проблемы при таможенном оформлении",
+    problemsProtocol1: "Протокол о нарушении таможенных правил",
+    transportDowntime1: "Затраты при простое транспорта",
+    corruption1: "Коррупция",
+    increaseCustomsValue1: "Необоснованное повышение таможенной стоимости",
+    codeChange1: "Изменение кода товара",
+
+    customsProblems2: "Возможные проблемы при таможенном оформлении",
+    problemsProtocol2: "Протокол о нарушении таможенных правил",
+    transportDowntime2: "Затраты при простое транспорта",
+    corruption2: "Коррупция",
+    increaseCustomsValue2: "Необоснованное повышение таможенной стоимости",
+    codeChange2: "Изменение кода товара",
+
+    avoidThis: "Хотите этого избежать?",
+
+    aboutSection: "О Нас",
+
+    about1: "Руз Консалтинг – это команда профессионалов с большим опытом работы в внешнеэкономической деятельности.",
+    about2: "Мы знаем все о таможенном оформлении любых товаров и транспортных средств. ",
+    about3: "За счет большого опыта работы наших специалистов, мы знаем все особенности работы таможенных органов и оперативно отслеживаем изменения законодательств разных стран.",
+    about4: "А все для того, чтобы предоставлять качественные услуги вам здесь и сейчас",
+
+    benefitsSection1: "Преимущества",
+    benefitsSection2: "работы с нами",
+    benefit1: "Более 15 лет опыта",
+    benefit2: "Принимаем оплату услуг после качественного их выполнения",
+    benefit3: "Представляем ваши интересы на всех этапах работы",
+    benefit4: "Оформляем любые товары «под ключ»",
+    benefit5: "Предлагаем выгодные условия постоянным клиентам",
+    benefit6: "Доступная цена",
+    benefit7: "Поможем в оптимизации налогообложения",
+
+    sevicesSection1: "Что",
+    sevicesSection2: "входит",
+    sevicesSection3: "в услугу?",
+
+    services1: "Консультации по любым вопросам таможенного оформления и оптимизации налогообложения",
+    services2: "Получением всех необходимых документов от контролирующих органов",
+    services3: "Классификация товаров",
+    services4: "Декларирование ваших товаров таможенным органам",
+    services5: "Представление ваших интересов на всех этапах таможенного оформления",
+
+    processesSection1: "Процесс",
+    processesSection2: "работы",
+
+    processesStep1: "Шаг 1",
+    processesStep1d: "Заключаем договор на оказание услуг и получаем документы на груз",
+    processesStep2: "Шаг 2",
+    processesStep2d: "Анализируем документы и определяем меры тарифного и нетарифного регулирования",
+    processesStep3: "Шаг 3",
+    processesStep3d: "Получаем разрешительные документы от других контролирующих органов",
+    processesStep4: "Шаг 4",
+    processesStep4d: "Оформляем груз",
+
+    needABroker: "Нужен надежный таможенный брокер?",
+    ruzHappyToHelp: "Руз Консалтинг всегда рад помочь!",
+
+    copyrightRuz: "Ⓒ Руз Консалтинг",
+    llcRuz: "ООО “Руз Консалтинг”",
+    adressRuz: "Республика Таджикистан <br> 734000 Душанбе <br>  5/5 Ул. Н.Мухаммад <br> 233 офис, БЦ “Регус”",
+    yourCargo: "Ваш груз — наша забота!",
+
+   },
+ 
+};
+setLanguage(getCookie('lang') || 'ru');
+
+
+function setLanguage(language) {
+
+  var x = getCookie('lang');
+  if (x) {
+    if (x != language) {
+      eraseCookie('lang');
+      setCookie('lang', language, 7);
+    }
+  } else {
+    setCookie('lang', language, 7);
+  }
+  document.documentElement.lang = language;
+  var currentLanguage = getCookie('lang');
+
+  app.lang = app.translations[currentLanguage] || app.translations.en;
+
+  changeLanguageSwitchFunction(currentLanguage);
+//   changeLogo(currentLanguage);
+  for (const property in app.lang) {
+
+    if (document.getElementById(property) != null) {
+
+      document.getElementById(property).innerHTML = app.lang[property];
+    //   document.getElementById(property).style.fontFamily = 'OpenSans';
+    }
+  }
+}
+
+function changeLanguageSwitchFunction(language) {
+    if(language == "en") {
+        document.getElementById("languageSwitch").setAttribute('onclick', "setLanguage('ru')");
+    } 
+    else if (language == "ru") {
+        document.getElementById("languageSwitch").setAttribute('onclick', "setLanguage('en')");
+    }
+}
+
+function setCookie(name, value, days) {
+  var expires = "";
+  if (days) {
+    var date = new Date();
+    date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+    expires = "; expires=" + date.toUTCString();
+  }
+  document.cookie = name + "=" + (value || "") + expires + "; path=/";
+}
+function getCookie(name) {
+  var nameEQ = name + "=";
+  var ca = document.cookie.split(';');
+  for (var i = 0; i < ca.length; i++) {
+    var c = ca[i];
+    while (c.charAt(0) == ' ') c = c.substring(1, c.length);
+    if (c.indexOf(nameEQ) == 0) return c.substring(nameEQ.length, c.length);
+  }
+  return null;
+}
+function eraseCookie(name) {
+  document.cookie = name + '=; Path=/; Expires=Thu, 01 Jan 1970 00:00:01 GMT;';
+}
